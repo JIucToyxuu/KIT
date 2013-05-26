ef615a0576167ff46eafc5db62cf9933be793ae3
Anton <anton@anton-eME732.(none)>
2013-05-26 14:11:32 +0400

    full functions for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,21 @@
 class CommentsController < ApplicationController
-	before_filter :signed_in_user, only: :destroy
+	before_filter :signed_in_user, only: [:destroy, :create]
 	
+	def create
+  		@comment = current_user.comments.build(params[:comment])
+    	if @comment.save
+      		flash[:success] = "Commentt created!"
+      		redirect_to root_url
+    	else
+    		flash[:success] = "Micropost NOOOOO created!"
+      		redirect_to root_url     
+    	end
+    end
+
 	def destroy
 		Comment.find(params[:id]).destroy
 		redirect_to request.referer
 	end
+
 
 end
