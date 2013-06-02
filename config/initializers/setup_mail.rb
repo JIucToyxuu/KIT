57a8e57dccca723e17004c055ef73e8478c9477e
Anton <anton@anton-eME732.(none)>
2013-06-02 17:25:08 +0400

    test mail 3

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => "domain.com",
+   :domain               => "blooming-badlands-7238.herokuapp.com",
    :user_name            => "odmin.srstu.2013@yandex.ru",
    :password             => "123456",
    :authentication       => "plain",
