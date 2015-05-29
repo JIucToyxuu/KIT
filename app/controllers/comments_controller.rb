65f640d74ce69a95c475042f9ad8b1440e5377f4
jiuctoyxuu <antonduzhnov.90@gmail.com>
2015-05-29 14:22:04 +0300

    refactoring code style --skip-ci

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
-	before_filter :signed_in_user, only: [:destroy, :create]
-	
-	def create
-  		@comment = current_user.comments.build(params[:comment])
-    	if @comment.save
-      		flash[:success] = "Comment created!"
-      		redirect_to root_url
-    	else
-    		flash[:success] = "Micropost NOOOOO created!"
-      		redirect_to root_url     
-    	end
+  before_filter :signed_in_user, only: [:destroy, :create]
+  
+  def create
+      @comment = current_user.comments.build(params[:comment])
+      if @comment.save
+          flash[:success] = "Comment created!"
+          redirect_to root_url
+      else
+        flash[:success] = "Micropost NOOOOO created!"
+          redirect_to root_url
+      end
     end
 
-	def destroy
-		Comment.find(params[:id]).destroy
-		redirect_to request.referer
-	end
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to request.referer
+  end
 
 
 end
